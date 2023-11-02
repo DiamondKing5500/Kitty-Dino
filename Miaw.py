6c041a787fff239621fc4b7b295a4e5a0f553f48
DiamondKing5500 <149483211+DiamondKing5500@users.noreply.github.com>
2023-11-02 20:44:12 +0700

    Create Miaw.py

---
+++ Miaw.py
@@ -0,0 +1,21 @@
+# print('Miaw to friends Roar to enemies.')
+# print('From Kittysauraus')
+
+import random
+
+Banban = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+Password = ""
+
+for i in range():
+    Password += random.choice(Banban)
+
+print(Password)
+
+
+
+
+
+
+
+
+
